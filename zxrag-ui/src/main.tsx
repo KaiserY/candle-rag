48abb8cd06c39bd6373e902f20f89113e62d4f83
KaiserY <yueyang.fanqiang@gmail.com>
2024-03-07 18:07:13 +0800

    wip

--- zxrag-ui/src/main.tsx
+++ zxrag-ui/src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import "./i18n.ts";
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
